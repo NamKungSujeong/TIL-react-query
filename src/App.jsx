6d60547b18518d62d7b00d0214b7c3fd9d1a0bad
NamKungSujeong <nksj98@naver.com>
2024-05-26 23:20:19 +0900

    쿼리 클라이언트 및 공급자 추가하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Posts } from "./Posts";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+// 새 쿼리클라이언트를 만들 수 있게 해줌
+// 괄호안에 옵션을 넣을 수 있음
 
 function App() {
   return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
