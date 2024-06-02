766422831fee497bb802a2a0b0848dc1f1bbd2fd
NamKungSujeong <nksj98@naver.com>
2024-06-02 19:28:25 +0900

    12. 페이지네이션/13. 프리패칭'

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // 1. useQuery 불러오기
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  // 2. useQuery 실행하기
+  const queryClient = useQueryClient();
+
   const { data, isError, isLoading, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
     staleTime: 2000,
   });
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
 
   if (isLoading) {
     return <h3>Loaindg...</h3>;
@@ -44,11 +54,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage(currentPage - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+        >
           Next page
         </button>
       </div>
