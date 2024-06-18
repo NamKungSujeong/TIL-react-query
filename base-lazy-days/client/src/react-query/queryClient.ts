9743e3aad36bf7bdcac206e2481a57259e9df4eb
NamKungSujeong <nksj98@naver.com>
2024-06-19 08:51:41 +0900

    39. 쿼리 클라이언트에 대한 onError 기본 값

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,20 +1,28 @@
 import { toast } from "@/components/app/toast";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
 
-// function errorHandler(errorMsg: string) {
-//   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-//   // one message per page load, not one message per query
-//   // the user doesn't care that there were three failed queries on the staff page
-//   //    (staff, treatments, user)
-//   const id = "react-query-toast";
+function errorHandler(errorMsg: string) {
+  // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
+  // one message per page load, not one message per query
+  // the user doesn't care that there were three failed queries on the staff page
+  //    (staff, treatments, user)
+  const id = "react-query-toast";
+  // 중복된 토스트 메세지가 표시되지 않도록 id 설정
 
-//   if (!toast.isActive(id)) {
-//     const action = "fetch";
-//     const title = `could not ${action} data: ${
-//       errorMsg ?? "error connecting to server"
-//     }`;
-//     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-//   }
-// }
+  if (!toast.isActive(id)) {
+    const action = "fetch";
+    const title = `could not ${action} data: ${
+      errorMsg ?? "error connecting to server"
+    }`;
+    toast({ id, title, status: "error", variant: "subtle", isClosable: true });
+  }
+}
 
-export const queryClient = new QueryClient();
+// errorHandler를 뭐리태시 옵션에 제공해주기
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      errorHandler(error.message);
+    },
+  }),
+});
