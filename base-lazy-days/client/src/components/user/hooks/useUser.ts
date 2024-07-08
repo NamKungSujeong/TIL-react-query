17dbd97412fb3f0bfae64fdb02c34cca554dfb50
NamKungSujeong <nksj98@naver.com>
2024-07-08 21:39:15 +0900

    useUser에 useQuery 호출 추가

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
@@ -5,22 +6,33 @@ import type { User } from "@shared/types";
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { generateUserKey } from "@/react-query/key-factory";
 
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  // userId : 서버에서 어떤 사용자 정보를 요청하는지 알려주기 위함
+  // userToken : JWT 헤더를 전달하고 해당 정보에 대한 권한이 있음을 증명하기 위함
+  // 이 두개의 값은 useLoginData에서 가지고 올 것
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${userId}`,
+    {
+      headers: getJWTHeader(userToken),
+    }
+  );
 
-//   return data.user;
-// }
+  return data.user;
+}
 
 export function useUser() {
+  const { userId, userToken } = useLoginData();
   // TODO: call useQuery to update user data from server
-  const user: User = null;
+  const { data: user } = useQuery({
+    enabled: !!userId,
+    queryKey: generateUserKey(userId, userToken),
+    queryFn: () => getUser(userId, userToken),
+    staleTime: Infinity,
+    // 데이터는 가비지 컬렉션 시간이 만료되지 않는 한 다시 가져오지 않으며 사용자가 스스로 업데이트할 경우에만 변경
+  });
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
