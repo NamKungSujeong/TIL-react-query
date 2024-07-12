3c43afcbb9af5673e0cae557366accaf42ca400d
NamKungSujeong <nksj98@naver.com>
2024-07-12 23:02:43 +0900

    변이 훅 useReserveAppointtment 작성

--- base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,3 +1,5 @@
+import { useMutation } from "@tanstack/react-query";
+
 import { Appointment } from "@shared/types";
 
 import { useLoginData } from "@/auth/AuthContext";
@@ -5,26 +7,35 @@ import { axiosInstance } from "@/axiosInstance";
 import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 import { queryKeys } from "@/react-query/constants";
 
+// 예약을 위한 변이함수를 반환하는 훅
+
 // for when we need functions for useMutation
-// async function setAppointmentUser(
-//   appointment: Appointment,
-//   userId: number | undefined,
-// ): Promise<void> {
-//   if (!userId) return;
-//   const patchOp = appointment.userId ? 'replace' : 'add';
-//   const patchData = [{ op: patchOp, path: '/userId', value: userId }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function setAppointmentUser(
+  appointment: Appointment,
+  userId: number | undefined
+): Promise<void> {
+  if (!userId) return;
+  const patchOp = appointment.userId ? "replace" : "add";
+  const patchData = [{ op: patchOp, path: "/userId", value: userId }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
 export function useReserveAppointment() {
   const { userId } = useLoginData();
 
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  //이 함수를 통해 우리의 캘린더는 예약과 함께
+  //변이 함수를 실행하여 사용자를 위해 해당 예약을 업데이트할 수 있음
+  const { mutate } = useMutation({
+    mutationFn: (appointment: Appointment) =>
+      setAppointmentUser(appointment, userId),
+    onSuccess: () => {
+      toast({ title: "You have reserved  an appointment!", status: "success" });
+    },
+  });
+
+  return mutate;
 }
