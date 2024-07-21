67db2926bfad9326d012aaf2689cd954583dfd13
NamKungSujeong <nksj98@naver.com>
2024-07-21 22:24:27 +0900

    테스트에서의 쿼리 클라이언트와 제공자

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@/test-utils";
 
 import { Treatments } from "../Treatments";
 
 test("renders response from query", () => {
   // write test here
+  // 테스팅 라이브러리에서 테스트를 작성하거나 시작하는 방식은 컴포넌트를 렌더링하는 것
+  render(<Treatments />);
 });
